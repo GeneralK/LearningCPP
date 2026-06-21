0c2c4318fe254088d845bb1735f1450f22bc2244
Dev <7538710+GeneralK@users.noreply.github.com>
2026-06-21 15:19:58 -0400

    Added pet class with starting health & hunger as private value. Added public function. constructor WIP

--- PetSim/PetSim.cpp
+++ PetSim/PetSim.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+class Pet {
+private:
+	string petName;
+	int health = 100;
+	int hunger = 100;
+
+public:
+	int updateHealth() {
+		health += 1;
+	}
+	Pet() {
+		//need to think of something i guess for construfctor
+	}
+
+	void namePet() {
+		cout << "Choose name of pet: ";
+		cin >> petName;
+		cout << "Perfect, your pet is now named: "
+			<< petName
+			<< "!\nHave a good day\n";
+	}
+};
+
 int main() {
-	cout << "test/n";
+	Pet pet1;
+	pet1.namePet();
+	return 0;
 }
