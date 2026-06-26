193915ac6b3ba26e378f6c9303fdce2187330d38
Dev <7538710+GeneralK@users.noreply.github.com>
2026-06-25 21:19:47 -0400

    Added enum Stats in Pet class. Created Activities class for future expansion.

--- PetSim/PetSim.cpp
+++ PetSim/PetSim.cpp
@@ -6,11 +6,20 @@ private:
 	string petName;
 	int health = 100;
 	int hunger = 100;
+	int hygiene = 50;
+
 
 public:
+	enum Stats {
+		statHealth,
+		statHunger,
+		statHygiene,
+	};
+
 	int updateHealth() {
 		health += 1;
 	}
+
 	Pet() {
 		//need to think of something i guess for construfctor
 	}
@@ -22,10 +31,34 @@ public:
 			<< petName
 			<< "!\nHave a good day\n";
 	}
+
+	void cleanPet(int activity) {
+		hygiene += activity;
+	}
+
+	int displayStats(Stats stat) {
+		switch (stat) {
+		case statHealth: return health;
+		case statHunger: return hunger;
+		case statHygiene: return hygiene;
+		}
+		return 0;
+	}
+
+};
+
+class Activities {
+public:
+	void shower(Pet& pet) {
+		pet.cleanPet(25);
+	}
 };
 
 int main() {
 	Pet pet1;
 	pet1.namePet();
+	Activities activities;
+	activities.shower(pet1);
+	cout << pet1.displayStats(Pet::statHygiene);
 	return 0;
 }
