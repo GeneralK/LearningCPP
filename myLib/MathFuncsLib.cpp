bfa287d475000586572cd2d87490c58bae10edbe
Dev <7538710+GeneralK@users.noreply.github.com>
2026-07-07 21:43:29 -0400

    Made the Calculator.cpp use the code from MSDN (Microsoft Developer Network)  Walkthrough: Creating and Using a Static Library (C++)

---
+++ myLib/MathFuncsLib.cpp
@@ -0,0 +1,32 @@
+// MathFuncsLib.cpp
+// compile with: /c /EHsc
+// post-build command: lib MathFuncsLib.obj
+#include "pch.h"
+#include "MathFuncsLib.h"
+
+#include <stdexcept>
+
+using namespace std;
+
+namespace MathFuncs
+{
+    double MyMathFuncs::Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    double MyMathFuncs::Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    double MyMathFuncs::Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    double MyMathFuncs::Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
