2eb3bd6c323b1bcdac647cdb12c60fc94e4beaba
Dev <7538710+GeneralK@users.noreply.github.com>
2026-07-25 14:12:32 -0400

    Added preset answers to magicalBall class.

--- Magic8Ball/Magic8Ball.cpp
+++ Magic8Ball/Magic8Ball.cpp
@@ -1,7 +1,38 @@
 /*
-Task: Create a magic 8 ball that outputs specific strings
+Task: Create a magic 8 ball that outputs specific strings.
+Project idea from codecademy.com
 
 */
 
 #include <string>
 #include <iostream>
+
+class magicBall {
+	std::string answer1 = "It is certain.";
+	std::string answer2 = "It is decidedly so.";
+	std::string answer3 = "Without a doubt.";
+	std::string answer4 = "Yes - definitely.";
+	std::string answer5 = "You may rely on it.";
+	std::string answer6 = "As I see it, yes.";
+	std::string answer7 = "Most likely.";
+	std::string answer8 = "Outlook good.";
+	std::string answer9 = "Yes";
+	std::string answer10 = "Signs point to yes.";
+	std::string answer11 = "Reply hazy, try again.";
+	std::string answer12 = "Ask again later";
+	std::string answer13= "Better not tell you now.";
+	std::string answer14= "Cannot predict now.";
+	std::string answer15= "Concentrate and ask again.";
+	std::string answer16= "Don't count on it.";
+	std::string answer17 = "My reply is no.";
+	std::string answer18 = "My sources say no.";
+	std::string answer19 = "Outlook not so good.";
+	std::string answer20 = "Very doubtful.";
+
+
+};
+
+int main() {
+	magicBall ball1;
+
+}
