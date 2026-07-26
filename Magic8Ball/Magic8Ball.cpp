d6e7b156b939b3181a95d164230f9eae3418bf23
Dev <7538710+GeneralK@users.noreply.github.com>
2026-07-26 10:53:00 -0400

    Updated Cheatsheet & lesson learned content. Changed magicalBall string member content. Made it an array so I can roll the answers more easily.

--- Magic8Ball/Magic8Ball.cpp
+++ Magic8Ball/Magic8Ball.cpp
@@ -6,33 +6,41 @@ Project idea from codecademy.com
 
 #include <string>
 #include <iostream>
+#include <random>
 
 class magicBall {
-	std::string answer1 = "It is certain.";
-	std::string answer2 = "It is decidedly so.";
-	std::string answer3 = "Without a doubt.";
-	std::string answer4 = "Yes - definitely.";
-	std::string answer5 = "You may rely on it.";
-	std::string answer6 = "As I see it, yes.";
-	std::string answer7 = "Most likely.";
-	std::string answer8 = "Outlook good.";
-	std::string answer9 = "Yes";
-	std::string answer10 = "Signs point to yes.";
-	std::string answer11 = "Reply hazy, try again.";
-	std::string answer12 = "Ask again later";
-	std::string answer13= "Better not tell you now.";
-	std::string answer14= "Cannot predict now.";
-	std::string answer15= "Concentrate and ask again.";
-	std::string answer16= "Don't count on it.";
-	std::string answer17 = "My reply is no.";
-	std::string answer18 = "My sources say no.";
-	std::string answer19 = "Outlook not so good.";
-	std::string answer20 = "Very doubtful.";
+	std::string answer[20]{
+		"It is certain.",
+		"It is decidedly so.",
+		"Without a doubt.",
+		"Yes - definitely.",
+		"You may rely on it.",
+		"As I see it, yes.",
+		"Most likely.",
+		"Outlook good.",
+		"Yes",
+		"Signs point to yes.",
+		"Reply hazy, try again.",
+		"Ask again later",
+		"Better not tell you now.",
+		"Cannot predict now.",
+		"Concentrate and ask again.",
+		"Don't count on it.",
+		"My reply is no.",
+		"My sources say no.",
+		"Outlook not so good.",
+		"Very doubtful.",
+	};
 
-
+	void ballReply() {
+		//should I randomize on top of the standard c++ randomize
+		std::cout << answer[1];
+	}
 };
 
 int main() {
 	magicBall ball1;
+	ball1.ballReply();
+
 
 }
