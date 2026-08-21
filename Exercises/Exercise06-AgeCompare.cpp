a95f7d7dee8460fe9bacd9a28e01ca774be6be6e
Dev <7538710+GeneralK@users.noreply.github.com>
2026-08-21 11:03:58 -0400

    Exercise 06: WIP - Commiting framework of the project. Strcut for containing person age and name. Basic function to compare age.

--- Exercises/Exercise06-AgeCompare.cpp
+++ Exercises/Exercise06-AgeCompare.cpp
@@ -11,3 +11,30 @@ David Jenkins (age 44) is older than John Bacon (age 37).
 
 #include <iostream>
 #include <format>
+
+struct person {
+	int age{ 0 };
+	std::string name{ 0 };
+
+	void inputInfo() {
+		std::cout << "Gimme name";
+		std::cin >> this->name;
+		std::cout << "Gimme age";
+		std::cin >> this->age;
+	}
+};
+
+void comparePersonAge(person a, person b) {
+	if (a.age > b.age) {
+		std::cout << std::format ("{} (age {}) is older than {} ( age {})", a.name,a.age,b.name,b.age);
+	}
+};
+
+void Exercise06() {
+	person person1;
+	person person2;
+	person1.inputInfo();
+	person2.inputInfo();
+	comparePersonAge(person1, person2);
+
+}
