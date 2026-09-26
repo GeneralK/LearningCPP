09e2a31cb6bfb4b487621b4e7768f8415ba5501c
Dev <7538710+GeneralK@users.noreply.github.com>
2026-09-26 10:55:56 -0400

    Exer11: Reworking the code to include other loop exercise in the same lesson. (LearnCPP 8.8 Loops & While Statement) Added a menu.

--- Exercises/Exercise11-SimpleLoop.cpp
+++ Exercises/Exercise11-SimpleLoop.cpp
@@ -5,23 +5,55 @@ Use a loop variable of type char.
 LearnCPP 8.8 introduction-to-loops-and-while-statements
 */
 
-/*
-Loop to display character/letter. Then also display ascii.
-97 - 122 are a-z
-*/
 
 #include <iostream>
+#include <format>
 using std::cout;
+using std::cin;
 
 void Exercise11() {
-	char charCount{ 97 };
-	while (charCount <= 122) {
-		cout << charCount;
-		cout << " " << static_cast <int> (charCount) << "\n";
-		++charCount;
-	}
-	cout << "DONE!";
+	loopExercise::menue();
 }
 
+namespace loopExercise {
+	void menue() {
+		const int EXIT_MENU{ 5 }; // Which option is for exiting menu. Modify as necessary
+		int menueChoice{ 0 };
+	cout << std::format(
+		R"(Which function to run?
+				1: Display a-z & their ASCII codes
+				2: Delete Student
+				3: Find by Name
+				4: Find by Roll
+				5: Exit
+			
+				Enter a choice:)"
+	);
+	cin >> menueChoice;
+	switch (menueChoice) {
+	case 1:
+		char charCount{ 97 };
+		while (charCount <= 122) {
+			cout << charCount;
+			cout << " " << static_cast <int> (charCount) << "\n";
+			++charCount;
+		}
+		cout << "DONE!";
+		break;
+	case 2:
+		//
+		break;
+	case 3:
+		//
+		break;
+	case 4:
+		//
+		break;
+	case EXIT_MENU:
+		//Exits Program
+		break;
+	}
 
+}
+}
 
